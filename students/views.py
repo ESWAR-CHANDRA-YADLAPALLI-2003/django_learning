a5a2984eb3c4126df93e1908d992fcea44a749f8
ESWAR-CHANDRA-YADLAPALLI-2003 <160381924+ESWAR-CHANDRA-YADLAPALLI-2003@users.noreply.github.com>
2025-11-13 06:59:17 +0530

    Day-3 Django Learning: Models, Admin Panel, Database ORM operations, and Template Integration

---
+++ students/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Student
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'students/students_list.html', {'students': students})
