a4b4578821f4c96de40750b8b59a672fda1d527f
ESWAR-CHANDRA-YADLAPALLI-2003 <160381924+ESWAR-CHANDRA-YADLAPALLI-2003@users.noreply.github.com>
2025-11-21 06:36:09 +0530

    Day-4: Added student model updates, migrations, CRUD practice

--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import Student
+from .models import Course, Student
 
-admin.site.register(Student)
+#admin.site.register(Course)
+#admin.site.register(Student)
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'course', 'marks')
--- students/models.py
+++ students/models.py
@@ -1,11 +1,23 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Student(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=40)
     age = models.IntegerField()
     email = models.EmailField()
-    city = models.CharField(max_length=50)
+    marks = models.IntegerField(null=True, blank=True)
+    joined_on = models.DateTimeField(auto_now_add=True)
+
+    # Relationship field
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True)
+
 
     def __str__(self):
         return self.name
